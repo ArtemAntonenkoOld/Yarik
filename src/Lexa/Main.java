452f2cd9f24f2d479213c6560423fb74ad93e8ac
GoGo228 <ghhfghhf5@gmail.com>
2016-10-18 11:47:18 +0300

    lul

--- src/Lexa/Main.java
+++ src/Lexa/Main.java
@@ -5,14 +5,26 @@ import java.util.Scanner;
 
     public class Main {
         public static void main(String[] args) {
-            //l();
+            l();
             p();
+            app4();
         }
         static int inputInt(String inp) {
             while (true) {
                 try {
                     Scanner sk = new Scanner(System.in);
                     System.out.println(inp);
+                    return sk.nextInt();
+                } catch (InputMismatchException ex) {
+                    System.err.println("Invalid Data");
+                }
+            }
+        }
+        static int inputInt() {
+            while (true) {
+                try {
+                    Scanner sk = new Scanner(System.in);
+
                     return sk.nextInt();
                 } catch (InputMismatchException ex) {
                     System.err.println("Invalid Data");
@@ -64,6 +76,36 @@ import java.util.Scanner;
             }
             nope/=2;
             System.out.println("est takie pari "+nope);
+        }
+        static void app4(){
+            int k=inputInt();//размер
+            int j=inputInt();
+            int mnoj=1;
+            int z=0;
+            int zstart= inputInt();//начало
+            int zend = inputInt(); //konez
+            zstart-=1;
+            zend=-1;
+
+            double[][] arr;
+            arr = new double[k][j];
+            for (int i = 0; i <k ; i++) {
+                for (int l = 0; l <j ; l++) {
+                    arr[i][l]=Math.round(Math.random()*10000);
+                    System.out.print(arr[i][l]+ " ");
+                }
+                System.out.println();
+            }
+
+            for (int i = 0; i <k ; i++) {
+                for (int l = 0; l <j ; l++) {
+                    if (z>=zstart && z<=zend){
+                        mnoj*=arr[i][l];
+                    }
+                    z++;
+                }
+            }
+            System.out.println(mnoj);
         }
     }
 
